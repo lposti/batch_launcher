a3b62589b1650e45d2f97d4ff750d60a07416bf3
lposti <lorenzo.posti@gmail.com>
2015-07-29 16:13:13 +0200

    introducing "_mode_hernquist_h_eq_g_chi1"

--- pbs.py
+++ pbs.py
@@ -1,6 +1,6 @@
 __author__ = 'lposti'
 
-from numpy import linspace, append, meshgrid, reshape
+from numpy import linspace, append, meshgrid, reshape, ones
 from subprocess import call
 
 
@@ -12,16 +12,23 @@ class ModelJobs(object):
         self.num_jobs = self.num * self.num
         self.iter = 0
 
-        self.model_type = "Hernquist"
-        x = append(linspace(0.2, 1., num=num/2, endpoint=False), linspace(1, 5, num=num/2))
-        y = append(linspace(0.2, 1., num=num/2, endpoint=False), linspace(1, 5, num=num/2))
-
-        self.dz, self.dphi = meshgrid(x, y)
-        self.dz, self.dphi = reshape(self.dz, self.num_jobs), reshape(self.dphi, self.num_jobs)
+        self.model_type, self.dz, self.dphi, self.chi = self._mode_hernquist_h_eq_g_chi1()
 
         self.input_dir = "inputfiles/"
         self.jobs_dir = "jobfiles/"
         self.basename = ""
+
+    def _mode_hernquist_h_eq_g_chi1(self,):
+
+        model_type = "Hernquist"
+        x = append(linspace(0.2, 1., num=self.num_jobs/2, endpoint=False), linspace(1, 5, num=self.num_jobs/2))
+        y = append(linspace(0.2, 1., num=self.num_jobs/2, endpoint=False), linspace(1, 5, num=self.num_jobs/2))
+
+        chi = ones(self.num_jobs, dtype=float)
+        dz, dphi = meshgrid(x, y)
+        dz, dphi = reshape(dz, self.num_jobs), reshape(dphi, self.num_jobs)
+
+        return model_type, dz, dphi, chi
 
     def launch_jobs(self):
 
@@ -35,21 +42,21 @@ class ModelJobs(object):
 
     def print_input_file(self):
 
-        dphi, dz, chi = self.dphi[self.iter], self.dz[self.iter], 1.
+        dphi, dz, chi = self.dphi[self.iter], self.dz[self.iter], self.chi[self.iter]
         name = self._get_basename(dphi, dz, chi)
 
         f = open(self.input_dir+name+".par", 'w')
-        f.write("itermax   5\n")
-        f.write("model   "+self.model_type+"\n")
-        f.write("outfile   "+name+"\n")
-        f.write("h(J):dphi   "+str(dphi)+"\n")
-        f.write("h(J):dz   "+str(dz)+"\n")
-        f.write("g(J):dphi   "+str(dphi)+"\n")
-        f.write("g(J):dz   "+str(dz)+"\n")
-        f.write("chi   "+str(chi)+"\n")
-        f.write("mass   1.\n")
-        f.write("r0   1.\n")
-        f.write("q   1.\n")
+        f.write("itermax    5\n")
+        f.write("model      "+self.model_type+"\n")
+        f.write("outfile    "+name+"\n")
+        f.write("h(J):dphi  "+str(dphi)+"\n")
+        f.write("h(J):dz    "+str(dz)+"\n")
+        f.write("g(J):dphi  "+str(dphi)+"\n")
+        f.write("g(J):dz    "+str(dz)+"\n")
+        f.write("chi        "+str(chi)+"\n")
+        f.write("mass       1.\n")
+        f.write("r0         1.\n")
+        f.write("q          1.\n")
 
     def print_pbs_script(self):
 
@@ -63,10 +70,10 @@ class ModelJobs(object):
         f.write("#PBS -o "+self.basename+".out\n")
         f.write("#PBS -e "+self.basename+".err\n")
         f.write("#PBS -m a\n")
-        f.write("#PBS -M lorenzo.posti@gmail.com\n")
+        f.write("#PBS -M lorenzo.posti@gmail.com\n\n")
         f.write("cd /gpfs/work/IscrC_CALIFAfJ/fJmodels/\n")
         f.write("module load intel/cs-xe-2015--binary\n")
-        f.write("module load gsl/1.16--intel--cs-xe-2015--binary\n")
+        f.write("module load gsl/1.16--intel--cs-xe-2015--binary\n\n")
         f.write("./fJmodels "+self.input_dir+self.basename+".par \n")
 
         return pbs_script_name
